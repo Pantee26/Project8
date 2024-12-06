a86a6a7f1cff8cf487485d6401c2ed0f9186f65d
pantee26 <panteezaza@gmail.com>
2024-12-06 16:34:17 +0700

    "completed"

--- lab8_3.cpp
+++ lab8_3.cpp
@@ -1,13 +1,44 @@
-/*
-"Enter your age: "
-"Enter your height: "
-"Enter your bounty: "
-"Your character = "
-"Zoro"
-"Sanji"
-"Usopp"
-"Chopper"
-"Franky"
-"Brook"
-"Jinbe"
-*/
+#include <iostream>
+using namespace std;
+
+int main(){
+    int age;
+    double height, bounty;
+    string character;
+    cout << "Enter your age: ";
+    cin >> age;
+
+    if (age > 60) {
+        character = "Brook";
+    } else if (age <= 25) {
+        cout << "Enter your height: ";
+        cin >> height;
+
+        if (height < 100) {
+            character = "Chopper";
+        } else if (height < 180) {
+            character = "Usopp";
+        } else {
+            cout << "Enter your bounty: ";
+            cin >> bounty;
+
+            if (bounty > 1100000000) {
+                character = "Zoro";
+            } else {
+                character = "Sanji";
+            }
+        }
+    } else {
+        cout << "Enter your bounty: ";
+        cin >> bounty;
+
+        if (bounty > 500000000) {
+            character = "Jinbe";
+        } else {
+            character = "Franky";
+        }
+    }
+    cout << "Your character = " << character;
+
+    return 0;
+}
